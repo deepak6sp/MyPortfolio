fd2606d7ff34f6e5befdeb0c9b26c1555690a41d
Deepak Prakash <deepak6sp@users.noreply.github.com>
2016-03-06 10:40:08 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Deepak Portfolio | Web Developer</title>
+	<title>Deepak Portfolio | Web Developer</title> 
 	<meta charset="utf-8" />
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
